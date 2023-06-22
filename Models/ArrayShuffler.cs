dd4844a9d626f3e66fd8199c7f7ddda29792c8d3
Curllly <pavelllageev@gmail.com>
2023-06-23 02:25:38 +0500

    Question is done

---
+++ Models/ArrayShuffler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayShufle.Models
+{
+    public static class ArrayShuffler
+    {
+        public static T[] Shuffle<T>(T[] array)
+        {
+            Random rnd = new Random();
+
+            int n = array.Length;
+
+            for (int i = n - 1; i > 0; i--)
+            {
+                int newPos = rnd.Next(n);
+                T value = array[newPos];
+
+                array[newPos] = array[i];
+                array[i] = value;
+            }
+
+            return array;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using ArrayShufle.Models;
+
+int[] myArray = { 1, 2, 3, 4, 5 };
+string[] strings =  { "я", "люблю", "гороховый", "суп", };
+int[] testarray = { 0 };
+
+// перемешивание int
+foreach (int i in ArrayShuffler.Shuffle(myArray))
+{
+    Console.WriteLine(i);
+}
+
+Console.WriteLine();
+// перемешивание string
+foreach (string i in ArrayShuffler.Shuffle(strings))
+{
+    Console.WriteLine(i);
+}
+
+Console.WriteLine();
+// перемешивание Array
+Array[] arrays = { myArray, strings,  testarray};
+
+foreach (var item in ArrayShuffler.Shuffle(arrays))
+{
+    Console.WriteLine(item);
+}
